c0c43331a38fbe979ee08b5273a1893ae4492a4c
Farah Student <farah.gus@hotmail.com>
2019-05-15 15:55:44 -0400

    problem 11

--- exercises.js
+++ exercises.js
@@ -9,14 +9,84 @@
 
 // 6
 
-function filterMe(arr) {
+// function filterMe(arr) {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 5) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return console.log(newArr);
+// }
+
+// filterMe([1, 2, 3, 5, 6, 6, 7]);
+
+
+// function sumArr(arr){
+//   let newArr = [];
+//   let length = arr.length;
+//   let arrSum = 0;
+//   for (let j = 0; j < length; j++) {
+//     for (let i = 0; i < arr.length; i++) {
+//       arrSum += arr[i];
+//       newArr.push(arrSum);
+//     }
+//     arrSum = 0;
+//     arr.shift()
+//   }
+  
+//   console.log(Math.max(...newArr)); // 12
+// }
+
+// sumArr([4, 6, -3, 5, -2, 1])
+
+// 8 Merge arrays
+// function mergeAndSort(arr1, arr2) {
+//   arr1 = arr1.concat(arr2);
+//   arr1 = arr1.sort((a, b) => {
+//     return a - b;
+//   });
+
+//   console.log(arr1)
+// }
+
+// mergeAndSort([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]);
+
+
+
+// 9 Remove characters
+// function removeChar(str, characters){
+//   characters = characters.split('');
+//   for (let i = 0; i < characters.length; i ++) {
+//     let regex = new RegExp(characters[i], 'g')
+//     str = str.replace(regex, '');
+//   }
+//   console.log(str);
+// }
+
+// removeChar('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou');
+
+
+// 10 Products
+function products(arr){
+  let numProduct = 1;
   let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 5) {
-      newArr.push(arr[i]);
+  
+  for (let i = 0; i < arr.length; i ++) {
+   let productArray = []; 
+   productArray.push(arr);
+    
+    productArray.splice(i, 1);
+    console.log(productArray)
+    numProduct = 1;
+    for (let j = 0; j < productArray.length; j ++) {
+  
+      (numProduct *= productArray[j]);
+      newArr.push(numProduct);
     }
-  }
-  return console.log(newArr);
+  } 
+
+  console.log(newArr);
 }
 
-filterMe([1, 2, 3, 5, 6, 6, 7]);
+products([1, 3, 9, 4]);
